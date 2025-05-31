4d851e5af60b327d23c2f23a16c0917b92cc09da
Rachanabera <137858950+Rachanabera@users.noreply.github.com>
2025-05-31 20:06:51 +0530

    Initial Commit

---
+++ twosum.py
@@ -0,0 +1,11 @@
+nums = list(map(int,input("Enter the list of Numbers: ").split()))
+target = int(input("Enter target variable : "))
+
+def twosum(nums,target):
+    for i in range(0,len(nums)):
+        for j in range(i+1,len(nums)):
+            if nums[i]+nums[j]==target:
+                return [i,j]
+
+result = twosum(nums,target)
+print(f"Indices:{result}")
